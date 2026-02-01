e6c8aba8f0723698f6bb28bb2ad3f90cd6a9855d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-02-01 20:53:12 +0200

    ✍️ parse relationships

--- Sources/CloudKitCompilantClient/main.swift
+++ Sources/CloudKitCompilantClient/main.swift
@@ -2,11 +2,21 @@ import CloudKitCompilant
 import SwiftData
 
 @available(macOS 14, *)
+@Model
+class Job {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+@available(macOS 14, *)
 @Model @CloudKitCompilant
 class SampleModel {
     var name: String = ""
     var anotherSus: Int?
-    var optionalValue: Bool?
+    var optionalValue: Bool = false
+    @Relationship(deleteRule: .cascade) var jobs: [Job]?
     
     init(
         name: String,
@@ -15,6 +25,6 @@ class SampleModel {
     ) {
         self.name = name
         self.anotherSus = anotherSus
-        self.optionalValue = optionalValue
+        self.optionalValue = optionalValue ?? false
     }
 }
--- Sources/CloudKitCompilantMacros/CloudKitCompilantMacro.swift
+++ Sources/CloudKitCompilantMacros/CloudKitCompilantMacro.swift
@@ -9,7 +9,7 @@ public struct CloudKitCompilant {}
 extension CloudKitCompilant: ExtensionMacro {
     public typealias Error = CloudKitCompilantError
     public typealias Syntax = SwiftSyntax.ExtensionDeclSyntax
-    private typealias VarDeclInfo = (name: String, isOptional: Bool, hasDefaultValue: Bool)
+    private typealias VarDeclInfo = (name: String, isOptional: Bool, hasDefaultValue: Bool, isRelationship: Bool)
     
     public static func expansion(
         of node: SwiftSyntax.AttributeSyntax,
@@ -33,32 +33,55 @@ extension CloudKitCompilant: ExtensionMacro {
                 let name: String = binding.pattern.description
                 let hasInitializer: Bool = binding.initializer != nil
                 let isOptional: Bool = binding.typeAnnotation?.type.is(OptionalTypeSyntax.self) ?? false
+                let isRelationship: Bool = decl.attributes.contains(where: {
+                    // Attribute must be defined
+                    guard let attribute = $0.as(AttributeSyntax.self) else { return false }
+                    // Check if attribute name is Relationship
+                    return attribute.attributeName.description == "Relationship"
+                })
                 
-                return (name, isOptional, hasInitializer)
+                return (name, isOptional, hasInitializer, isRelationship)
             })
             .filter({ tuple in tuple != nil })
             .compactMap({ tuple in tuple! })
         
         // This is a list of fields that contain error.
         var missingDefaultValues: [String] = []
+        // All relations must be optional. Collect errors as well.
+        var nonOptionalRelations: [String] = []
         
         // All properties must be optional, or have default values.
         // So, there is only two options:
-        // - Optional variable
+        // - Optional variable (isOptional && !isRelationship)
+        // - Optional relationship (isOptional && isRelationship)
         // - Non-optional variable with default value
-        for (name, isOptional, hasDefaultValue) in variables {
-            // Optional values are valid anyways
-            if isOptional { continue }
-            // Value is non-optional. In that way it must have default value
-            if !hasDefaultValue {
+        for (name, isOptional, hasDefaultValue, isRelationship) in variables {            
+            // Catch non-optional relationship
+            if isRelationship && !isOptional {
+                nonOptionalRelations.append(name)
+                continue
+            }
+            // Found optional relationship. Skipping the step.
+            if isRelationship && isOptional { continue }
+            
+            // Catch non-optional value without default value
+            if !isRelationship && !isOptional && !hasDefaultValue {
                 missingDefaultValues.append(name)
                 continue
             }
+            // Found non-optinal value, that has default one. Skipping the step.
+            if !isRelationship && !isOptional && hasDefaultValue { continue }
+            
+            // Value is optional. Skipping the step
+            if !isRelationship && isOptional { continue }
         }
         
         // Report errors, if they exist
         guard missingDefaultValues.count == 0 else {
             throw Error.missingDefaultValue(modelName: coreModelName, properties: missingDefaultValues)
+        }
+        guard nonOptionalRelations.count == 0 else {
+            throw Error.nonOptionalRelationship(modelName: coreModelName, properties: nonOptionalRelations)
         }
         
         return []
@@ -67,13 +90,17 @@ extension CloudKitCompilant: ExtensionMacro {
 
 public enum CloudKitCompilantError: Error {
     case missingDefaultValue(modelName: String, properties: [String])
+    case nonOptionalRelationship(modelName: String, properties: [String])
 }
 
 extension CloudKitCompilantError: DiagnosticMessage {
     public var message: String {
         switch self {
         case .missingDefaultValue(let modelName, let properties):
-            return "Model \(modelName) has required fields without default values: \(properties.joined(separator: ", "))"
+            return "Model \(modelName) has required fields without default values: \(properties.joined(separator: ", "))."
+            
+        case .nonOptionalRelationship(let modelName, let properties):
+            return "Model \(modelName) has non-optional relationships: \(properties.joined(separator: ", ")). All relationships must be optional."
         }
     }
     
@@ -81,13 +108,13 @@ extension CloudKitCompilantError: DiagnosticMessage {
         switch self {
         case .missingDefaultValue(let modelName, _):
             MessageID(domain: modelName, id: "missing_default_values_on_model")
+        case .nonOptionalRelationship(let modelName, _):
+            MessageID(domain: modelName, id: "have_non_optional_relationships_on_model")
         }
     }
     
     public var severity: DiagnosticSeverity {
-        switch self {
-            case .missingDefaultValue: return .error
-        }
+        .error
     }
 }
 
