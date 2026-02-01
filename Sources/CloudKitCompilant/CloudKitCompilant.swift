1889f7a79bc0ee69b2ef9651baea90f45781d11f
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-02-01 20:58:20 +0200

    ✍️ update comment

--- Sources/CloudKitCompilant/CloudKitCompilant.swift
+++ Sources/CloudKitCompilant/CloudKitCompilant.swift
@@ -1,7 +1,20 @@
 import SwiftData
 
-#warning("TODO: Update comment below")
 /**
+ This macro parses model declaration at checks if it is ready for CloudKit deploy.
+ 
+ ```swift
+ @Model @CloudKitCompilant
+ class SampleModel {
+     var requiredField: String = ""
+     var requiredField2: String // Will error. All required fields must have default values.
+     var optional: Int?
+     @Relationship(deleteRule: .cascade) var jobs: [Job]?
+     @Relationship(deleteRule: .cascade) var jobs2: [Job] // Will error. Relationships must be optional.
+     
+     ...
+ }
+ ```
  */
 @attached(extension, conformances: Observable, PersistentModel, Sendable)
 public macro CloudKitCompilant() = #externalMacro(module: "CloudKitCompilantMacros", type: "CloudKitCompilant")
